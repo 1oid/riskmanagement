6d23fe831b6b40c9932ca57982cadce80c90b66a
loid <2563152103@qq.com>
2021-04-09 23:38:01 +0800

    修复智慧分析逻辑

--- stu/views_/intellianalysis.py
+++ stu/views_/intellianalysis.py
@@ -49,38 +49,94 @@ class AnalysisView(View):
 
     def get(self, request, page=1, did=None):
         objects = IntelliAnalysis.objects.all()
-
-        # count = objects.count()
-
         results = []
-        result_set = []
+
+        # 过滤重复名字
+        item_sets = {}
+        reply_objects = []
+        not_reply_objects = []          # 用来保存不重复的元素
 
         for item in objects:
-            # 拒绝重复
-            _ = "{}_{}".format(item.md5, item.action)
+            _filename = item.filename.strip().split("\\")[-1].strip()
 
-            if _ in result_set:
+            if item_sets.get(_filename, None):
+                item_sets.get(_filename).append(item)
                 continue
 
-            result_set.append(_)
-            _cache = CacheFile.objects.filter(identifier=item.did, MD5=item.md5, IsImge=1).last()
+            item_sets[_filename] = [item]
 
-            print(_cache)
-            results.append({
-                    "id": item.id,
-                    "did": get_computer_name_by_id(item.did),
-                    "odid": item.did,
-                    "filename": item.filename.split("\\")[-1],
-                    "filepath": item.filename,
-                    "filesize": self.size_format(item.filesize),
-                    "md5": item.md5,
-                    "description": item.description,
-                    "duplicate": 1 if item.duplicate else 0,
-                    "action": item.get_action_string(),
-                    "other": item.Other.strftime("%Y-%m-%d %H:%I:%S"),
-                    "is_img": _cache.IsImge if _cache else 0,
-                    "save_name": "/image/{}/{}".format(_cache.identifier, _cache.MD5) if _cache else ""
-                })
+        for _, values in item_sets.items():
+            cache_list = []
+            first_item = {}
+            cache_count = 0
+
+            for index, value in enumerate(values):
+
+                _cache = CacheFile.objects.filter(identifier=value.did, MD5=value.md5, IsImge=1).last()
+
+                if index == 0:
+                    first_item = {
+                        "id": value.id,
+                        "odid": get_computer_name_by_id(value.did),
+                        "did": value.did,
+                        "filename": value.filename.split("\\")[-1],
+                        "filesize": self.size_format(value.filesize),
+                        "remark": value.description[:40],
+                        "md5": value.md5,
+                        "cache_one": 0,
+                        "cache_all": [],
+                        "action": value.get_action_string(),
+                        "cache_all_count": 0,
+                        "other": value.Other.strftime("%Y-%m-%d %H:%M:%S")
+                    }
+
+                if _cache:
+                    first_item['cache_all_count'] = first_item['cache_all_count'] + 1
+
+                    cache_list.append({
+                            "id": _cache.id,
+                            "cache_name": _cache.CacheName,
+                            "save_name": "/image/{}/{}".format(_cache.identifier, _cache.MD5),
+                            "is_img": _cache.IsImge,
+                            "filename": value.filename,
+                            "other": value.Other.strftime("%Y-%m-%d %H:%M:%S")
+                        })
+
+                # print(len(values), index + 1)
+                if len(values) == index + 1:
+                    first_item['cache_all'] = cache_list
+                    results.append(first_item)
+
+        # count = objects.count()
+        # results = []
+        # result_set = []
+        #
+        # for item in objects:
+        #     # 拒绝重复
+        #     _ = "{}_{}".format(item.md5, item.action)
+        #
+        #     if _ in result_set:
+        #         continue
+        #
+        #     result_set.append(_)
+        #     _cache = CacheFile.objects.filter(identifier=item.did, MD5=item.md5, IsImge=1).last()
+        #
+        #     print(_cache)
+        #     results.append({
+        #             "id": item.id,
+        #             "did": get_computer_name_by_id(item.did),
+        #             "odid": item.did,
+        #             "filename": item.filename.split("\\")[-1],
+        #             "filepath": item.filename,
+        #             "filesize": self.size_format(item.filesize),
+        #             "md5": item.md5,
+        #             "description": item.description,
+        #             "duplicate": 1 if item.duplicate else 0,
+        #             "action": item.get_action_string(),
+        #             "other": item.Other.strftime("%Y-%m-%d %H:%I:%S"),
+        #             "is_img": _cache.IsImge if _cache else 0,
+        #             "save_name": "/image/{}/{}".format(_cache.identifier, _cache.MD5) if _cache else ""
+        #         })
 
         p = Paginator(results, 10)
         obj = {
